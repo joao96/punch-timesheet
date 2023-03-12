45bd2ce8617a5d730703cdf57d9a66108f8b113c
joao96 <jvpoletti@gmail.com>
2023-03-12 19:38:31 -0300

    ♻️ refactor: change import order

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { DatabaseModule } from '@infra/database/database.module';
 import { HttpModule } from '@infra/http/http.module';
 import { ErrorMiddleware } from '@infra/http/middlewares/error-middleware';
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 
 @Module({
   imports: [HttpModule, DatabaseModule],
