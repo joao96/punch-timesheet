dd227bf05a2912010a5d99009ff094caa6831c3b
joao96 <jvpoletti@gmail.com>
2023-03-12 19:12:03 -0300

    ♻️ refactor(app): add constants

---
+++ src/application/constants/index.ts
@@ -0,0 +1,5 @@
+export const WORKDAY_HOURS = 8; // hours
+export const SATURDAY = 6;
+export const SUNDAY = 0;
+export const PUNCHES_A_DAY = 4;
+export const LUNCH_BREAK_TIME = 60; // minutes
---
+++ src/application/types/status.ts
@@ -0,0 +1,11 @@
+export const ConstStatusCode = {
+  OK: 200,
+  Created: 201,
+  NoContent: 204,
+  BadRequest: 400,
+  Unauthorized: 401,
+  Forbidden: 403,
+  Conflict: 409,
+  NotFound: 404,
+  ServerError: 500,
+};
